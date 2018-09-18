e3251af5162099c1a87c75958c9e74dabf9c6aa2
403 <403@DESKTOP-IPJFIU0>
2018-09-18 14:59:23 +0900

    2018.09.18

---
+++ src/my/day03/Add.java
@@ -0,0 +1,17 @@
+package my.day03;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// 100과 200을 더하는 프로그램
+		int x;
+		int y;
+		int sum;	// 합을 저장하는 변수
+		
+		x=100;
+		y=200;
+		sum=x+y;
+		System.out.println("100과 200의 합=" + sum);
+	}
+
+}
